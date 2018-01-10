d0a74208feb1eb23045757a0f7a7eb508e073c15
jhornsberger <jhornsberger@gmail.com>
2018-01-10 11:33:38 -0800

    Only backup files that install.sh would overwrite

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ############################
-# .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
@@ -13,20 +12,29 @@ files="ackrc agignore ctags gitconfig inputrc my.cnf tmux vim zsh dircolors"
 
 ##########
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+   if [ -e ~/.$file ]
+   then
+      if [ -h ~/.$file ]
+      then
+         echo "Removing existing symlink ~/.$file"
+         rm ~/.$file
+      else
+         if [ ! -e $olddir ]
+         then
+            # create dotfiles_old in homedir
+            echo "Creating $olddir for backup of existing dotfiles"
+            mkdir -p $olddir
+         fi
+         echo "Moving existing ~/.$file to $olddir"
+         mv ~/.$file $olddir/$file
+      fi
+   fi
+   echo "Creating symlink to $file in home directory."
+   ln -s $dir/$file ~/.$file
 done
